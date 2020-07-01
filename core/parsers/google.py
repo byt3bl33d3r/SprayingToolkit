a231dd34d5cbd5fc30ae586db8fd4ebfc4ff91a4
TheTechromancer <thetechromancer@protonmail.com>
2020-07-01 15:24:58 -0400

    fixed bug where google and bing html parsing outputs blank string

--- core/parsers/google.py
+++ core/parsers/google.py
@@ -1,13 +1,18 @@
-import lxml.html
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+
 from core.parsers.linkedin import linkedin_se_name_parser
 
 
 def google(content):
     names = []
-    html = lxml.html.fromstring(content)
+    html = BeautifulSoup(content)
 
-    for text in html.xpath('//h3[@class="LC20lb"]//text()'):
+    for text in [e.text for e in html.body.findAll('h3', attrs={'class':'LC20lb'})]:
         first, last = linkedin_se_name_parser(text)
-        names.append((first, last, text))
+        if first or last:
+            names.append((first, last, text))
 
     return names
